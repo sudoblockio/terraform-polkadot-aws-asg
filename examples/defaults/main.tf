9dfc8e287c1db41d5dbcf40cee0fc4841e905500
Richard Mah <dick.mah@gmail.com>
2020-05-03 21:16:49 -0400

    Update default test

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -21,10 +21,7 @@ module "defaults" {
   stage       = "test"
 
   public_key             = var.public_key
-  relay_node_ip          = "1.2.3.4"
-  relay_node_p2p_address = "stuff.things"
   security_groups        = [module.network.sentry_security_group_id]
   subnet_ids             = module.network.public_subnets
   lb_target_group_arn    = module.lb.lb_target_group_arn
 }
-
