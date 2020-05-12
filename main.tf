0582caafbae79860a9bf4bc45b80bd53cf046a9b
rob <robcannonxyz@gmail.com>
2020-05-12 11:16:41 -0700

    cleanup [skip ci]

--- main.tf
+++ main.tf
@@ -1,19 +1,5 @@
 data "aws_region" "this" {}
 
-//module "label" {
-//  source = "github.com/robc-io/terraform-null-label.git?ref=0.16.1"
-//  tags = {
-//    NetworkName = var.network_name
-//    Owner       = var.owner
-//    Terraform   = true
-//    VpcType     = "main"
-//  }
-//
-//  environment = var.environment
-//  namespace   = var.namespace
-//  stage       = var.stage
-//}
-
 resource "null_resource" "requirements" {
   triggers = {
     time = timestamp()
