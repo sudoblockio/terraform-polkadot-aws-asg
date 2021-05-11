f7b3f32e0c8d98d8f02be4b9b4cbb51ed2381bcb
Richard Mah <dick.mah@gmail.com>
2021-05-11 01:25:52 -0400

    Add ansible all enabled test

---
+++ examples/standalone-all-ansible/outputs.tf
@@ -0,0 +1,15 @@
+output "tags" {
+  value = module.defaults.tags
+}
+
+output "name" {
+  value = module.defaults.name
+}
+
+output "public_ips" {
+  value = module.defaults.public_ips
+}
+
+output "dns_name" {
+  value = module.defaults.dns_name
+}
